c7ac0ff08a7b1a2cd35674a8dbe2dc922f39661b
Guido van de Riet <guidovdriet@gmail.com>
2019-04-02 15:12:30 +0200

    Update

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Platform, Image, StyleSheet, View } from "react-native";
-// import { MapView } from "expo";
 import MapView, { Marker } from "react-native-maps";
+import styled from "styled-components";
 
 export default class HomeScreen extends React.Component {
   static navigationOptions = {
@@ -50,7 +50,7 @@ export default class HomeScreen extends React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
+      <Container>
         <MapView
           style={{ flex: 1 }}
           initialRegion={{
@@ -61,111 +61,43 @@ export default class HomeScreen extends React.Component {
           }}
         >
           {this.state.markers.map((marker, i) => (
-            <Marker
-              key={i.toString()}
-              coordinate={marker.latlng}
-              title={marker.title}
-              description={marker.description}
-              // image={require("../assets/images/alert.png")}
-              // style={{ width: 20, height: 20 }}
-            >
-              <Image
-                source={require("../assets/images/alert.png")}
-                style={{ width: marker.width, height: marker.height }}
-              />
-            </Marker>
+            <>
+              <Marker
+                coordinate={marker.latlng}
+                title={marker.title}
+                description={marker.description}
+              >
+                <Image
+                  source={require("../assets/images/alert.png")}
+                  style={{ width: marker.width, height: marker.height }}
+                />
+              </Marker>
+            </>
           ))}
+          <NotificationContainer>
+            <Notification />
+          </NotificationContainer>
         </MapView>
-      </View>
+      </Container>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff"
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: "rgba(0,0,0,0.4)",
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: "center"
-  },
-  contentContainer: {
-    paddingTop: 30
-  },
-  welcomeContainer: {
-    alignItems: "center",
-    marginTop: 10,
-    marginBottom: 20
-  },
-  welcomeImage: {
-    width: 100,
-    height: 80,
-    resizeMode: "contain",
-    marginTop: 3,
-    marginLeft: -10
-  },
-  getStartedContainer: {
-    alignItems: "center",
-    marginHorizontal: 50
-  },
-  homeScreenFilename: {
-    marginVertical: 7
-  },
-  codeHighlightText: {
-    color: "rgba(96,100,109, 0.8)"
-  },
-  codeHighlightContainer: {
-    backgroundColor: "rgba(0,0,0,0.05)",
-    borderRadius: 3,
-    paddingHorizontal: 4
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: "rgba(96,100,109, 1)",
-    lineHeight: 24,
-    textAlign: "center"
-  },
-  tabBarInfoContainer: {
-    position: "absolute",
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: "black",
-        shadowOffset: { height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3
-      },
-      android: {
-        elevation: 20
-      }
-    }),
-    alignItems: "center",
-    backgroundColor: "#fbfbfb",
-    paddingVertical: 20
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: "rgba(96,100,109, 1)",
-    textAlign: "center"
-  },
-  navigationFilename: {
-    marginTop: 5
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: "center"
-  },
-  helpLink: {
-    paddingVertical: 15
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: "#2e78b7"
-  }
-});
+const Container = styled.View`
+  flex: 1;
+  background: #fff;
+`;
+
+const NotificationContainer = styled.View`
+  flex: 1;
+  height: 100%;
+  background: red;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const Notification = styled.View`
+  width: 100%;
+  height: 60%;
+  background: #fff;
+`;
